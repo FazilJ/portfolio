24fb2a0ecac9571a783495102217379480530726
FazilJ <jfazil777@gmail.com>
2025-02-19 10:00:07 -0800

    due to some changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -51,7 +51,7 @@ const Home = () => {
           </div>
           {/* Button */}
           <div className="bg-[#fe5617] px-5 py-2 rounded-2xl font-bold ">
-            <a href="/src/Assets/Capture001.png" download="Mohammed_Fazil_CV.png">Download CV</a>
+            <a href="/src/Assets/Mohammed_Fazil_Resume.jpeg" download="Mohammed_Fazil_CV.png">Download CV</a>
           </div>
         </div>
 
